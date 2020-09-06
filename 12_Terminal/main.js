4b2924ccd2a2f5989d90c431bc970f25d1175240
Pedro Gonzalez <pgonzalez@post.com>
2020-09-05 20:07:14 -0500

    Clase 12 Terminal

---
+++ 12_Terminal/main.js
@@ -0,0 +1,16 @@
+let saludo = 'Saludos desde la terminal!';
+
+console.log(saludo)
+
+// ------------------------------- Comandos -------------------------
+// pwd -> muestra la ruta en donde nos encontramos
+// ls -> muestra qué hay dentro de la ruta donde estoy
+// cd -> cambio de carpeta (cd nombre-carpeta o cd .. nos regresa a una carpeta anterior)
+// touch -> crea un archivo (touch nombre-archivo)
+// mkdir -> crea carpeta (mkdir nombre-carpeta)
+// nano -> editor de texto dentro de la terminal (nano archivo)
+// cat -> nos muestra que contenido tiene un archivo (cat archivo)
+// mv -> mover un archivo de lugar o cambiarle el nombre (mv archivo nueva-locacion o mv archivo nuevo-nombre)
+// rm -> borra archivo o carpeta (rm archivo o rm -r carpeta)
+// rm -rf -> borra todo (mucho cuidado con este comando)
+// clear -> limpia la pantalla
